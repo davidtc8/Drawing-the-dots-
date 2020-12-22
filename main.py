5eafb2e06b6c894f33994ae957b796b63df8686f
davidtc8 <davidtorresc8@gmail.com>
2020-12-21 21:06:53 -0600

    Recreating a painting project

--- main.py
+++ main.py
@@ -1,14 +1,58 @@
-# import colorgram
+from turtle import Turtle, Screen
+from color_list import color_list
+import random
 
-# colors = colorgram.extract('hirst_image.jpg', 30)
-# rgb_colors = []
+# this part of the project is where we recreate the painting
+tim = Turtle()
+screen = Screen()
+iterations = 0
+screen.bgcolor("#f7eeee")
+tim.hideturtle()
 
-#for color in colors:
-#    r = color.rgb.r
-#    g = color.rgb.g
-#    b = color.rgb.b
-#    new_color = (r, g, b)
-#    rgb_colors.append(new_color)
+def circles(color):
+    tim.pendown()
+    tim.color(color)
+    tim.fillcolor((color))
+    #tim.begin_fill()
+    tim.dot(15, color)
+    #tim.end_fill()
+    tim.penup()
+    tim.forward(35)
 
-# print(rgb_colors)
-color_list = [(199, 175, 117), (124, 36, 24), (210, 221, 213), (168, 106, 57), (222, 224, 227), (186, 158, 53), (6, 57, 83), (109, 67, 85), (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48), (9, 67, 47), (90, 141, 53), (181, 96, 79), (132, 40, 42), (210, 200, 151), (141, 171, 155), (179, 201, 186), (172, 153, 159), (212, 183, 177), (176, 198, 203), (150, 115, 120), (202, 185, 190), (40, 72, 82), (46, 73, 62), (47, 66, 82)]
+def movement():
+    tim.penup()
+    global iterations
+    iterations += 25
+    axisx = -700
+    axisy = -350
+    tim.goto(axisx, axisy + iterations)
+
+def tim_movement():
+    global tim
+    global screen
+    global color_list
+    screen.colormode(255)
+    tim.speed("fastest")
+    tim.penup()
+    tim.goto(-700, -350)
+    tim.pendown()
+    dividend = 0
+    divisor = 10
+    remainder = 10
+    done = False
+    while not done:
+        random.shuffle(color_list)
+        for color in color_list:
+            dividend += .25
+            divisor += .25
+            if dividend == 300:
+                done = True
+                break
+            elif dividend % divisor == remainder:
+                remainder += 10
+                movement()
+            else:
+                circles(color)
+
+tim_movement()
+screen.exitonclick()
